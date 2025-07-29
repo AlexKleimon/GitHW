d6596eb35c44ad403cf3677cf314c59a1ccf4913
Alex <Utin97@mail.ru>
2025-07-29 22:53:19 +0300

    Fir Program.cs: add manual data entry

--- Program.cs
+++ Program.cs
@@ -1,9 +1,11 @@
-﻿internal class Program
+﻿using System.Data;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        int x = 5;
-        int y = 10;
+        int x = int.Parse(Console.ReadLine()?? "0");
+        int y = int.Parse(Console.ReadLine()?? "0");
         Console.WriteLine($"Sum nambers x: {x} and y: {y} = {x+y}");
     }
 }
